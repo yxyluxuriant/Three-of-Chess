c7c03f3bbd4672eabf1657a459054ccb7d0dbc65
yxyluxuriant <2209025178@qq.com>
2022-04-06 22:48:23 +0800

    提交代码

---
+++ game1/game1/game.h
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#define ROW			3
+#define COL			3
+
+void InitBoard(char board[ROW][COL],int row,int col);
+void DisplayBoard(char board[ROW][COL],int row,int col);
+void PlayrMove(char board[ROW][COL],int row,int col);
+void ComputerMove(char board[ROW][COL],int row,int col);
+
+//玩家赢 - '*'
+//电脑赢 - '#'
+//平局   - 'Q'
+//继续   - 'C'
+char IsWin(char board[ROW][COL],int row,int col);
